a17755f9bdb5a934d7aa6a45056b8724242afba2
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2023-02-18 15:15:39 -0500

    Implementing sorting functions in python

---
+++ MasterTheCodingInterview/4_Algorithms/2_Sorting/index.py
@@ -0,0 +1,27 @@
+# Sorting Algorithms
+arr = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+# Bubble Sort
+def bubble_sort():
+    pass
+print(bubble_sort(arr))
+
+# Selection Sort
+def selection_sort():
+    pass
+print(selection_sort(arr))
+
+# Insertion Sort
+def insertion_sort():
+    pass
+print(insertion_sort(arr))
+
+# Merge Sort
+def merge_sort():
+    pass
+print(merge_sort(arr))
+
+# Quick Sort
+def quick_sort():
+    pass
+print(quick_sort(arr))
